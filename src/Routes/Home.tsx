0a29e7b59fdd8d96bf02e8039ee0112b5e6e1245
JHKIMS <wjdgns5131@gmail.com>
2023-01-29 16:55:05 +0900

    HoonFlix: Api받는 타입스크립트 설정완료

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,26 @@
+import { useQuery } from "react-query";
+import { getMovies } from "../api";
+
+interface IMovie{
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface IGetMoviesResult{
+  dates: {
+    maximum: string;
+    minimum: string;
+    page:number;
+    result: IMovie[];
+    total_pages: number;
+    total_result: number;
+  };
+}
+
 function Home() {
+  const {data, isLoading} = useQuery(["movies","nowPlaying"], getMovies)
   return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
 }
 
